16d2ded1f9e024b40d630ef39306e032f794901b
KohLiWei <liwei.9898@gmail.com>
2019-11-14 10:36:22 +0800

    Initial

---
+++ app/src/main/java/com/example/exercise3/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.example.exercise3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(),AdapterView.OnItemSelectedListener {
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?,
+                                            view:View?,
+                                            position: Int,
+                                            id: Long){
+        /*val selectedItem = spinnerAge.selectedItemPosition*/
+        val selectedItem= spinnerAge.getItemAtPosition(position)
+        Toast.makeText( this,"Selected Item = "+ selectedItem, Toast.LENGTH_LONG).show()
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //Handling item selected listebner for the spinner
+        spinnerAge.onItemSelectedListener=this
+        buttonCalculate.setOnClickListener{
+            calculatePremium()
+        }
+
+    }
+    private fun calculatePremium(){
+        //get the age group
+        val age: Int=spinnerAge.selectedItemPosition
+        var extraMale=0
+        var extraSmoker=0
+
+         var premium = when(age){
+            0->60 //Less than 17
+             1-> 70 //17 to 25
+             2->90
+             3->120
+             else->150
+        }
+
+        // get the gender selection. ID of radio button
+        val gender = radioGroupGender.checkedRadioButtonId
+            if(gender==R.id.radioButtonMale){
+                extraMale =when(age){
+                    0->0
+                    1->50
+                    2->100
+                    3->150
+                    else->200
+                }
+
+            }
+
+
+        //Determine smoker or not
+        val smoker: Boolean = checkBoxSmoker.isChecked
+        if(smoker){
+            //calculate premium for smoker
+            extraSmoker = when(age){
+                0->0
+                1->100
+                2->150
+                3->200
+                4->250
+                else->300
+            }
+        }
+        var totalPremium=(extraSmoker+ premium + extraMale )
+        textViewPremium.text="RM${totalPremium}"
+    }
+
+    fun Reset(view: View) {
+        checkBoxSmoker.setChecked(false)
+        textViewPremium.text=null
+        spinnerAge.setSelection(0)
+        radioButtonMale.setChecked(false)
+        radioButtonFemale.setChecked(false)
+
+    }
+}
